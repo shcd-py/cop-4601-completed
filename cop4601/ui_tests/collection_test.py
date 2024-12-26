f39dccdccbe8a76dc689c125a6ab97441bb56de5
Şahin Hakan Can Doğan <183625567+shcd-py@users.noreply.github.com>
2024-12-26 03:17:33 +0300

    Update collection_test.py

--- cop4601/ui_tests/collection_test.py
+++ cop4601/ui_tests/collection_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 class ButtonTest:
@@ -8,8 +10,9 @@ class ButtonTest:
         self.driver = None
 
     def setup(self):
-        self.driver = webdriver.Edge(self.driver_path)
-        self.driver.maximize_window()
+        service = Service(self.driver_path) 
+        options = webdriver.EdgeOptions()
+        self.driver = webdriver.Edge(service=service, options=options)  
 
     def teardown(self):
         if self.driver:
@@ -20,7 +23,22 @@ class ButtonTest:
             self.setup()
             self.driver.get(url)
             time.sleep(2)
-            self.driver.find_element(By.ID, "doubleClickBtn").click()
-            time.sleep(2)
+            
+            action = ActionChains(self.driver)
+
+            # Double click button
+            double_click_button = self.driver.find_element(By.ID, "doubleClickBtn")
+            action.double_click(double_click_button).perform()
+            time.sleep(5)
+
+            # Right click button
+            right_click_button = self.driver.find_element(By.ID, "rightClickBtn")
+            action.context_click(right_click_button).perform()
+            time.sleep(7)
+
+            # Click me button
+            click_me_button = self.driver.find_element(By.ID, "Vg4CC")
+            click_me_button.click()
+            time.sleep(5)
         finally:
             self.teardown()
